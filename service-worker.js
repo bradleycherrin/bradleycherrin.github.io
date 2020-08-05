9aed328d00570333547f34bae89d11e4696acb6b
bradleycherrin <5648785+bradleycherrin@users.noreply.github.com>
2020-08-05 22:16:30 +0000

    Deploying to gh-pages from  @ 7d870b3adee067df542372d4426c3254c291656d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b5cb33ead710533aaafd9745afa24f31.js"
+  "/precache-manifest.3285173df8db33fe4b5e2f9490e92d87.js"
 );
 
 self.addEventListener('message', (event) => {
